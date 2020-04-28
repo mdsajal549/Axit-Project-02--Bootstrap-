c30f23314f113fcbd4bdf45b00e8db75e0eb7fee
mdsajal549 <sajalcse2018@gmail.com>
2020-04-28 17:59:52 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+});
+
+
+
+/*tooltip js*/
+ $('#btn').tooltip(top)
+        $('#btn-1').tooltip(top)
+        $('#btn-2').tooltip(top)
+
